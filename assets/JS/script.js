4ef9b2cd28fba61426129dd3bc5646240b9922d0
NathDCoder <themusicemperor@gmail.com>
2022-09-28 10:38:22 -0400

    TeamProject

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,6 +8,9 @@
 
 ////////// Api Call ///////////////////
 
+
+
+
 function getSearch(coffeeDrink){
     var URLAPI ='https://api.sampleapis.com/coffee/hot';
     $.ajax({
@@ -21,13 +24,21 @@ function getSearch(coffeeDrink){
         $(title).html(response[1].title)
         $(description).html(response[1].description)
         $(ingredients).html(response[1].ingredients)
-        
-
-
-
-
-
     })
+}
+
+//Second API for Nutritional Facts//
+
+
+async function sendApiRequest(){
+    let APP_ID  = "02c0a468"
+    let APP_KEY = "6f7f5bcc04cea5899844a678b604fc04"
+    let response = await fetch("https://api.edamam.com/api/nutrition-details");
+    console.log(response)
+}
+
+function useApiData(data){
+    
 }
 ////////////// ^^^^^^^^^ Need to adjust Dom based on search////////
 
