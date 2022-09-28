81c414b57c84869e0a92b63ef23ab9cb06d446c7
NathDCoder <themusicemperor@gmail.com>
2022-09-28 10:47:39 -0400

    EdamamApi

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -18,9 +18,9 @@ function getSearch(coffeeDrink){
         method: "GET"
     }).then(function(response){
         console.log(response);
-        var CoffeeImage =response[1].image;
-        $(image).html('<img src='+CoffeeImage+'>');
-        console.log(image)
+        // var CoffeeImage =response[1].image;
+        // $(image).html('<img src='+CoffeeImage+'>');
+        // console.log(image)
         $(title).html(response[1].title)
         $(description).html(response[1].description)
         $(ingredients).html(response[1].ingredients)
