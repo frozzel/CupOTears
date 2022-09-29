2c9200cdbad051ab4a40e14170719aa4613cb4c0
NathDCoder <themusicemperor@gmail.com>
2022-09-28 23:09:01 -0400

    api-testing

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,9 +8,6 @@
 
 ////////// Api Call ///////////////////
 
-
-
-
 function getSearch(coffeeDrink){
     var URLAPI ='https://api.sampleapis.com/coffee/hot';
     $.ajax({
@@ -31,21 +28,22 @@ function getSearch(coffeeDrink){
 
 ////////////// ^^^^^^^^^ Need to adjust Dom based on search////////
 ////////// Test new Api NutritionIX /////////////
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'c978dd31b1msh40737d18773c28ap1b2ec9jsn3a7dc7a1361d',
-        'X-RapidAPI-Host': 'nutritionix-api.p.rapidapi.com'
-    }
-};
 
-fetch('https://nutritionix-api.p.rapidapi.com/v1_1/search/coffee?fields=item_name%2Citem_id%2Cbrand_name%2Cnf_calories%2Cnf_total_fat', options)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
+
 ////// ^^^^^ check readme links ///////// example search "item_name,item_id,brand_name,nf_calories,nf_total_fat" //// 
 
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ddebcb2ceamsh1919faa5a84d055p10d063jsneee465e9522e',
+		'X-RapidAPI-Host': 'nutritionix-api.p.rapidapi.com'
+	}
+};
 
+fetch('https://nutritionix-api.p.rapidapi.com/v1_1/search/cappuccino?fields=item_name%2Cnf_calories%2Cnf_total_fat%2Citem_description', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
 
 
 getSearch()
