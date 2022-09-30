7b791ab333db77e0058a7837992cd031bc16207f
NathDCoder <themusicemperor@gmail.com>
2022-09-30 12:52:50 -0400

    CountdownMsg

---
+++ assets/JS/hidden.js
@@ -0,0 +1,14 @@
+//Message Countdown, variables for Countdown//
+let messages = ["Welcome", "to", "CupOTears.","Search", "your", "favorite", "coffee", "drink", "while", "you", "cry."];
+
+let delay = 2000;
+
+let coffeeMsg = document.getElementById("wordDisplay");
+//Function for countdown//
+messages.forEach(function(wordDisplay, i) {
+  setTimeout(function() {
+    coffeeMsg.innerHTML = wordDisplay;
+  }, delay * i);
+});
+//End Countdown//
+
