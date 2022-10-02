c243ca43b234fc55ca3e6586248aeca3d4916211
devWeibel <weibel.jason@gmail.com>
2022-10-02 13:16:01 -0400

    fixed error, shortened last fadeout a little so it faded faster, cleaned up file

--- assets/JS/hidden.js
+++ assets/JS/hidden.js
@@ -1,50 +1,38 @@
-// //Message Countdown, variables for Countdown//
-let messages = ["Welcome to CupOTears.","Search your favorite coffee", "while you cry."];
+//Message Countdown, variables for Countdown//
+let messages = ["Welcome to Cup O' Tears.", "Search your favorite coffee", "while you cry."];
+let delay = 2000;
+let coffeeMsg = document.getElementById("wordDisplay");
 
- let delay = 2000;
-
- let coffeeMsg = document.getElementById("wordDisplay");
- //Function for countdown//
- messages.forEach(function(wordDisplay, i) {
-  setTimeout(function() {
-  coffeeMsg.innerHTML = wordDisplay;
-   }, delay * i);
- });
+//Function for countdown//
+messages.forEach(function (wordDisplay, i) {
+  setTimeout(function () {
+    coffeeMsg.innerHTML = wordDisplay;
+  }, delay * i);
+});
 // //End Countdown//
 
- setTimeout(() => {
-    wordDisplay = document.getElementById('wordDisplay',[2]);
-  
-//     // 👇️ removes element from DOM
-   wordDisplay.style.display = 'none';
+setTimeout(() => {
+  wordDisplay = document.getElementById('wordDisplay', [2]);
+  //👇️ removes element from DOM
+  wordDisplay.style.display = 'none';
 
-//     // 👇️ hides element (still takes up space on page)
-     box.style.visibility = 'hidden';
-       }, 22000); // 👈️ time in milliseconds
-  
+  //👇️ hides element (still takes up space on page)
+  wordDisplay.style.visibility = 'hidden';
+}, 15000); // 👈️ time in milliseconds
 
+window.onload = fadeout;
 
-
-       window.onload=fadeout;
-           
-       function fadeout() {
+//wordDisplay fadeout
+function fadeout() {
   var fade = document.getElementById("wordDisplay");
-    
   var intervalID = setInterval(function () {
-        
-      if (!fade.style.opacity) {
-          fade.style.opacity = 1;
-      }
-        
-        
-      if (fade.style.opacity > 0) {
-          fade.style.opacity -= 0.009;
-      } 
-        
-      else {
-          clearInterval(intervalID);
-      }
-        
+
+    if (!fade.style.opacity) {
+      fade.style.opacity = 1;
+    } else if (fade.style.opacity > 0) {
+      fade.style.opacity -= 0.015;
+    } else {
+      clearInterval(intervalID);
+    }
   }, 200);
 }
-     
